b0cf0b61260c808b267c2ddee3855e149a47b2e2
lolomawisoft <lolomawisoft@gmail.com>
2018-11-28 13:25:30 +0100

    Update PWMexample.c
    added my own value to the example for further testing.
    next update will be in a new file

--- PWMexample.c
+++ PWMexample.c
@@ -1,7 +1,7 @@
 // source https://www.avrfreaks.net/forum/atmega8-16mhz-fast-pwm-50hz
-
-#include 
-#include 
+#define F_CPU 16000000
+#include <avr/io.h>
+#include <util/delay.h>
  
 int main(void)
 {
@@ -11,13 +11,14 @@ int main(void)
   TCCR1A = ((1 << COM1A1) | (1 << WGM11)); // clear OC1A on match + WGM mode 14 
   TCCR1B = ((1 << WGM12) | (1 << WGM13) | (1 << CS11)); // WGM mode 14 (Fast PWM)
 							// and 8x prescaler
-  ICR1  = 20000;  //set ICR1 to produce 50Hz frequency
-		  //(8000000 / 8 / 20000 = 50hz)
-  OCR1A = 1000;   // 20000 * 0.05 most left
+  ICR1  = 40000;  //set ICR1 to produce 50Hz frequency
+		  //(8000000 / 8 / 20000 = 50hz) <-- if you have divide on sure
+		// ill use my 16000000MHz so (16000000 / 8 / 40000 = 50 hz)
+  OCR1A = 2000;   // 40000 * 0.05 most left
 
   while (1)
     {
-      while (OCR1A++ < 2000) // cca 2 sec loop
+      while (OCR1A++ < 4000) // cca 2 sec loop
       	{
       	  _delay_ms(1);
       	}
